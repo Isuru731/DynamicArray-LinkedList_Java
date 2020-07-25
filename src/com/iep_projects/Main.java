f26d92e548a2248c32ae7b2ddf6baa19e7a5d6c0
Isuru Perera <isuruaaang@gmail.com>
2020-07-26 02:46:19 +0530

    Created LinkedList class

--- src/com/iep_projects/Main.java
+++ src/com/iep_projects/Main.java
@@ -3,14 +3,24 @@ package com.iep_projects;
 public class Main {
 
     public static void main(String[] args) {
-        Array numbers = new Array(3);
-        numbers.insert(10);
-        numbers.insert(20);
-        numbers.insert(30);
-        numbers.insert(40);
-        numbers.removeAt(1);
-        numbers.print();
-        System.out.println(numbers.indexOf(40));
+//        Array numbers = new Array(3);
+//        numbers.insert(10);
+//        numbers.insert(20);
+//        numbers.insert(30);
+//        numbers.insert(40);
+//        numbers.removeAt(1);
+//        numbers.print();
+//        System.out.println(numbers.indexOf(40));
+
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+//        list.removeFirst();
+        list.removeLast();
+        System.out.println(list.indexOf(50));
+        System.out.println(list.contains(50));
+
 
     }
 }
